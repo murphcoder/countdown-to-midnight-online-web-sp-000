eea2349e9b876bf86bbd793cdbb7228909c41815
Sean Murphy <filmmakersean@fastmail.net>
2019-05-02 23:59:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,10 @@
 
 def countdown(number)
   while number > 0
-    puts "#{number} SECOND(S)!"
+    return "#{number} SECOND(S)!"
     number -= 1
   end
+  return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
